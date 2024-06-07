f88505654714b19aeef7ad847fd21f92db62bd5f
KuRynnn <fahryan107@gmail.com>
2024-06-07 10:45:31 +0700

    fix test

--- src/test/java/com/mycompany/bangundatarbangunruangfull/KubusTest.java
+++ src/test/java/com/mycompany/bangundatarbangunruangfull/KubusTest.java
@@ -18,7 +18,7 @@ public class KubusTest {
     public void testKubus() {
         Kubus kubus = new Kubus(5, "Kubus Contoh");
         assertEquals(150.0, kubus.hitungLuasPermukaan(), 0.01);
-        assertEquals(150.0, kubus.hitungVolume(), 0.01);
+        assertEquals(125.0, kubus.hitungVolume(), 0.01);
     }
     
 }
